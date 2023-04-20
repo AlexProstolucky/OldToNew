a777405f94694f6626fd1c1da0d28f05a03ec3aa
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-20 21:54:36 +0300

    Добавьте файлы проекта.

---
+++ OldToNew/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = "..\\..\\..\\Program.cs";
+        string text;
+        using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                text = sr.ReadToEnd();
+            }
+        }
+        text = text.Replace("public", "private");
+        using (FileStream fs = new FileStream("new.cs", FileMode.Create, FileAccess.Write, FileShare.None))
+        {
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine(text);
+            }
+            Console.WriteLine("Text is sucessfully coppied and replaced!");
+            Console.ReadKey();
+        }
+    }
+}
